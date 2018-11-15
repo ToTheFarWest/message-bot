3014ffa147bf124b2d28277d56c454af69e88404
ToTheFarWest <yardenakin@gmail.com>
2018-11-16 01:07:53 +0200

    Added countdown feature

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const client = new Client()
 
 client.on("ready", () => {
   console.log(`Bot is ready to serve ${client.users.size} users on ${client.guilds.size} servers`)
-  main()
 })
 
 function main() {
-    setTimeout(() => {
-      client.channels.get(config.channel).send(config.message)
-      console.log("Message sent!")
-      main()
-    }, config.minutes * 60000)
-  }
+  setInterval(() => {
+    const deadline = config.placeholder
+    const t = getRemaining(config.deadline)
+    const timeLeft = `${t.days} days ${t.hours} hours ${t.minutes} minutes and ${t.seconds} seconds`
+    const message = config.message.replace(deadline, timeLeft)
+    client.channels.get(config.channel).send(message)
+    console.log("Message sent!")
+  }, config.minutes * 60000)
+}
 
-client.login(config.token)
+function getRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+  return {
+    total: total,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  }
+}
+
+client.login(config.token).then(() => {main()})
